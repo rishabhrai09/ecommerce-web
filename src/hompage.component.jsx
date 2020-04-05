17a46e8b3a62fcc3174c90820e620dd80e059338
rishabhrai09 <rishabh8526@gmail.com>
2020-04-05 15:56:44 +0530

    sass

---
+++ src/hompage.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+
+const Hompage=()=>(
+    <div className="homepage">
+        <div className="dicrectory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">Hats</h1>
+                    <span className="tittle`">shop now</span>
+
+                </div>
+
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">Cloths</h1>
+                    <span className="tittle`">shop now</span>
+
+                </div>
+
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">Shoes</h1>
+                    <span className="tittle`">shop now</span>
+
+                </div>
+
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">Electronics</h1>
+                    <span className="tittle`">shop now</span>
+
+                </div>
+                
+
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">Jackets</h1>
+                    <span className="spantittle`">shop now</span>
+
+                </div>
+                
+
+            </div>
+
+        </div>
+
+    </div>
+)
+    export default Hompage
