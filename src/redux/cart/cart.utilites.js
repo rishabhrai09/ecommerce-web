5252a2e849c43ebc33436303203f8d94c017529a
rishabhrai09 <rishabh8526@gmail.com>
2020-04-17 20:01:13 +0530

    add ,remove toggle

--- src/redux/cart/cart.utilites.js
+++ src/redux/cart/cart.utilites.js
@@ -17,13 +17,16 @@ export const addItemToCart=(cartItems,cartItemToAdd)=>{
 
 
 export const cartItemtoRemove=(cartItems,cartItemToRemove)=>{
-    const existingCartItem=cartItems.find(cartItem=>cartItem.id ===cartItemToRemove.id)
+    const existingCartItem =  cartItems.find(
+        cartItem=>cartItem.id ===cartItemToRemove.id)
 
-    if(existingCartItem.quantity===1){
-       return cartItems.filter(cartItem=>cartItem.id!==cartItemToRemove.id)
+    if(existingCartItem.quantity === 1){
+       return cartItems.filter(cartItem=>cartItem.id !== cartItemToRemove.id)
 
     }
-    return cartItems.map(cartItem=>cartItem.id ===cartItemtoRemove.id ?{...cartItem,quantity:cartItem.quantity-1}:cartItem)
+    return cartItems.map(cartItem=>
+        cartItem.id === cartItemtoRemove.id
+         ?{...cartItem,quantity:cartItem.quantity-1}:cartItem)
 
     
 }
